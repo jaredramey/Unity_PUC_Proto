fef14d08ce5ff998cc2675b206a2d02125f667ed
Jared Ramey <jared.ramey@students.aie.edu.au>
2015-12-20 22:47:32 -0800

    Script work
    Found a script but its depreciated. Have to fix a few lines up then it
    should work

--- PUC_Proto/Assets/Scripts/Player Controls/Camera_Movement.cs
+++ PUC_Proto/Assets/Scripts/Player Controls/Camera_Movement.cs
@@ -7,35 +7,74 @@ using System.Collections;
 * http://bit.ly/22bGQHB
 */
 
-    [System.Serializable]
-    public class Boundry
-    {
-        public float xMin, xMax, zMin, zMax;
-    }
-
-public class Camera_Movement : MonoBehaviour
+public class ExtendedFlycam : MonoBehaviour
 {
-    public float speed;
-    public float tilt;
-    public Boundry boundary;
 
-    void FixedUpdate()
+    /*
+	EXTENDED FLYCAM
+		Desi Quintans (CowfaceGames.com), 17 August 2012.
+		Based on FlyThrough.js by Slin (http://wiki.unity3d.com/index.php/FlyThrough), 17 May 2011.
+ 
+	LICENSE
+		Free as in speech, and free as in beer.
+ 
+	FEATURES
+		WASD/Arrows:    Movement
+		          Q:    Climb
+		          E:    Drop
+                      Shift:    Move faster
+                    Control:    Move slower
+                        End:    Toggle cursor locking to screen (you can also press Ctrl+P to toggle play mode on and off).
+	*/
+
+    public float cameraSensitivity = 90;
+    public float climbSpeed = 4;
+    public float normalMoveSpeed = 10;
+    public float slowMoveFactor = 0.25f;
+    public float fastMoveFactor = 3;
+
+    private float rotationX = 0.0f;
+    private float rotationY = 0.0f;
+
+    private CursorLockMode wantedMode;
+
+    void Start()
     {
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVerticle = Input.GetAxis("Vertical");
-
-        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVerticle);
-        
+        Cursor.lockState = wantedMode;
     }
 
-    //Saving this for possible later use
-    /*void SmoothLookAt()
+    void Update()
     {
-        Vector3 relPlayerPosition = player.position - transform.position;
+        rotationX += Input.GetAxis("Mouse X") * cameraSensitivity * Time.deltaTime;
+        rotationY += Input.GetAxis("Mouse Y") * cameraSensitivity * Time.deltaTime;
+        rotationY = Mathf.Clamp(rotationY, -90, 90);
 
-        Quaternion lookAtRoation = Quaternion.LookRotation(relPlayerPosition, Vector3.up);
+        transform.localRotation = Quaternion.AngleAxis(rotationX, Vector3.up);
+        transform.localRotation *= Quaternion.AngleAxis(rotationY, Vector3.left);
 
-        transform.rotation = Quaternion.Lerp(transform.rotation, lookAtRoation, smooth * Time.deltaTime);
-    }*/
+        if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
+        {
+            transform.position += transform.forward * (normalMoveSpeed * fastMoveFactor) * Input.GetAxis("Vertical") * Time.deltaTime;
+            transform.position += transform.right * (normalMoveSpeed * fastMoveFactor) * Input.GetAxis("Horizontal") * Time.deltaTime;
+        }
+        else if (Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.RightControl))
+        {
+            transform.position += transform.forward * (normalMoveSpeed * slowMoveFactor) * Input.GetAxis("Vertical") * Time.deltaTime;
+            transform.position += transform.right * (normalMoveSpeed * slowMoveFactor) * Input.GetAxis("Horizontal") * Time.deltaTime;
+        }
+        else
+        {
+            transform.position += transform.forward * normalMoveSpeed * Input.GetAxis("Vertical") * Time.deltaTime;
+            transform.position += transform.right * normalMoveSpeed * Input.GetAxis("Horizontal") * Time.deltaTime;
+        }
 
+
+        if (Input.GetKey(KeyCode.Q)) { transform.position += transform.up * climbSpeed * Time.deltaTime; }
+        if (Input.GetKey(KeyCode.E)) { transform.position -= transform.up * climbSpeed * Time.deltaTime; }
+
+        if (Input.GetKeyDown(KeyCode.End))
+        {
+            Screen.lockCursor = (Screen.lockCursor == false) ? true : false;
+        }
+    }
 }
