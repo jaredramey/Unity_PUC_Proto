1dd53487901d65a055b5fd724abc58041427a08c
Jared Ramey <rameyjared@gmail.com>
2016-03-02 17:44:09 -0800

    Changed camera controls and started UI work

--- PUC_Proto/Assets/Scripts/Player Controls/Camera_Movement.cs
+++ PUC_Proto/Assets/Scripts/Player Controls/Camera_Movement.cs
@@ -81,6 +81,9 @@ public class ExtendedFlycam : MonoBehaviour
 
 public class Camera_Movement : MonoBehaviour
 {
+    
+    public Vector3 TargetPos = new Vector3(0, 0, 0);
+
 
     public float cameraSensitivity = 90;
     public float climbSpeed = 4;
@@ -96,40 +99,58 @@ public class Camera_Movement : MonoBehaviour
     void Start()
     {
         Cursor.lockState = wantedMode;
+        transform.LookAt(TargetPos);
     }
 
     void Update()
     {
-        rotationX += Input.GetAxis("Mouse X") * cameraSensitivity * Time.deltaTime;
-        rotationY += Input.GetAxis("Mouse Y") * cameraSensitivity * Time.deltaTime;
-        rotationY = Mathf.Clamp(rotationY, -90, 90);
+        RotateAroundSun();
 
-        transform.localRotation = Quaternion.AngleAxis(rotationX, Vector3.up);
-        transform.localRotation *= Quaternion.AngleAxis(rotationY, Vector3.left);
+        //rotationX += Input.GetAxis("Mouse X") * cameraSensitivity * Time.deltaTime;
+        //rotationY += Input.GetAxis("Mouse Y") * cameraSensitivity * Time.deltaTime;
+        //rotationY = Mathf.Clamp(rotationY, -90, 90);
 
-        if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
+        //transform.localRotation = Quaternion.AngleAxis(rotationX, Vector3.up);
+        //transform.localRotation *= Quaternion.AngleAxis(rotationY, Vector3.left);
+
+        //if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
+        //{
+        //    transform.position += transform.forward * (normalMoveSpeed * fastMoveFactor) * Input.GetAxis("Vertical") * Time.deltaTime;
+        //    transform.position += transform.right * (normalMoveSpeed * fastMoveFactor) * Input.GetAxis("Horizontal") * Time.deltaTime;
+        //}
+        //else if (Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.RightControl))
+        //{
+        //    transform.position += transform.forward * (normalMoveSpeed * slowMoveFactor) * Input.GetAxis("Vertical") * Time.deltaTime;
+        //    transform.position += transform.right * (normalMoveSpeed * slowMoveFactor) * Input.GetAxis("Horizontal") * Time.deltaTime;
+        //}
+        //else
+        //{
+        //    transform.position += transform.forward * normalMoveSpeed * Input.GetAxis("Vertical") * Time.deltaTime;
+        //    transform.position += transform.right * normalMoveSpeed * Input.GetAxis("Horizontal") * Time.deltaTime;
+        //}
+
+
+        //if (Input.GetKey(KeyCode.Space)) { transform.position += transform.up * climbSpeed * Time.deltaTime; }
+        //if (Input.GetKey(KeyCode.LeftControl)) { transform.position -= transform.up * climbSpeed * Time.deltaTime; }
+
+        //if (Input.GetKeyDown(KeyCode.End))
+        //{
+        //    Screen.lockCursor = (Screen.lockCursor == false) ? true : false;
+        //}
+    }
+
+    void RotateAroundSun()
+    {
+        if (Input.GetKey(KeyCode.A))
         {
-            transform.position += transform.forward * (normalMoveSpeed * fastMoveFactor) * Input.GetAxis("Vertical") * Time.deltaTime;
-            transform.position += transform.right * (normalMoveSpeed * fastMoveFactor) * Input.GetAxis("Horizontal") * Time.deltaTime;
-        }
-        else if (Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.RightControl))
-        {
-            transform.position += transform.forward * (normalMoveSpeed * slowMoveFactor) * Input.GetAxis("Vertical") * Time.deltaTime;
-            transform.position += transform.right * (normalMoveSpeed * slowMoveFactor) * Input.GetAxis("Horizontal") * Time.deltaTime;
-        }
-        else
-        {
-            transform.position += transform.forward * normalMoveSpeed * Input.GetAxis("Vertical") * Time.deltaTime;
-            transform.position += transform.right * normalMoveSpeed * Input.GetAxis("Horizontal") * Time.deltaTime;
+            transform.RotateAround(TargetPos, new Vector3(0.0f, 0.1f, 0.0f), 20 * Time.deltaTime * normalMoveSpeed);
         }
 
-
-        if (Input.GetKey(KeyCode.Space)) { transform.position += transform.up * climbSpeed * Time.deltaTime; }
-        if (Input.GetKey(KeyCode.LeftControl)) { transform.position -= transform.up * climbSpeed * Time.deltaTime; }
-
-        if (Input.GetKeyDown(KeyCode.End))
+        else if(Input.GetKey(KeyCode.D))
         {
-            Screen.lockCursor = (Screen.lockCursor == false) ? true : false;
+            transform.RotateAround(TargetPos, new Vector3(0.0f, 0.1f, 0.0f), 20 * Time.deltaTime * -normalMoveSpeed);
         }
+
+        transform.Translate(Vector3.forward * Input.GetAxis("Mouse ScrollWheel") * (normalMoveSpeed * 10));
     }
 }
